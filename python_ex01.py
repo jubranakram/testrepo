b584634073f054d4d41efc6cf410b4f0a73d77ae
Jubran Akram <35421084+jubranakram@users.noreply.github.com>
2021-11-13 11:08:21 -0700

    added python_ex01
    added python_ex01 for printing greetings

---
+++ python_ex01.py
@@ -0,0 +1,22 @@
+'''
+Greetings from Python
+---------------------
+author: Jubran Akram
+'''
+
+def greetings(name='stranger'):
+  '''
+  Print greetings for the user on the screen.
+  
+  Args:
+  
+  name (str) : username
+  
+  
+  Returns:
+  
+  '''
+  print(f'Hello, {name}! Welcome to the IBM Full-stack developer training...')
+  
+if __name__ == '__main__':
+  greetings('Jubran')
